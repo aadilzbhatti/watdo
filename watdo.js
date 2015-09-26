b2e07f768365a4e43c46ee146cade0043272bac1
Aadil Bhatti <aadilzbhatti@gmail.com>
2015-09-26 17:06:33 -0500

    added parsing but not working yet

--- watdo.js
+++ watdo.js
@@ -3,7 +3,22 @@ Events = new Mongo.Collection("events");
 if (Meteor.isClient) {
   Template.body.helpers({
     events: function() {
-      return Events.find({});
+    	$.get("events.xml", function (data) {
+    		$(data).find("item").each(function () { 
+       			var el = $(this);
+        		Events.insert({
+        			title: el.find("title").text(),
+        			date: el.find("pubdate").text(),
+        			link: el.find("link").text(),
+        			description: el.find("description").text()
+        		});
+        		console.log("------------------------");
+        		console.log("title      : " + el.find("title").text());
+        		console.log("date     : " + el.find("pubdate").text());
+        		console.log("description: " + el.find("description").text());
+    		});
+		});
+      	return Events.find({});
     }
   });
 }

6a390654b1ed76e8bf9887778b1f7a01b047d640
Aadil Bhatti <aadilzbhatti@gmail.com>
2015-09-26 17:52:21 -0500

    lala

--- watdo.js
+++ watdo.js
@@ -1,32 +1,25 @@
 Events = new Mongo.Collection("events");
 
 if (Meteor.isClient) {
-  Template.body.helpers({
-    events: function() {
-    	$.get("events.xml", function (data) {
-    		$(data).find("item").each(function () { 
-       			var el = $(this);
-        		Events.insert({
-        			title: el.find("title").text(),
-        			date: el.find("pubdate").text(),
-        			link: el.find("link").text(),
-        			description: el.find("description").text()
-        		});
-        		console.log("------------------------");
-        		console.log("title      : " + el.find("title").text());
-        		console.log("date     : " + el.find("pubdate").text());
-        		console.log("description: " + el.find("description").text());
-    		});
-		});
-      	return Events.find({});
-    }
-  });
+	Template.body.helpers({
+    	events: function() {
+			$.getFeed({
+				url: "events.xml",
+				success: function(feed) {
+					feed.find('item').each(function() {
+						var el = $(this);
+						console.log(el.find("title").text());
+					});
+				}
+			});
+   		}
+  	});
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+	Meteor.startup(function () {
+    	// code to run on server at startup
+	});
 }
 
 function eventful_search() {

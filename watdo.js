1f1fd791da65862725d574074773bdec2c9e91de
Aadil Bhatti <aadilzbhatti@gmail.com>
2015-09-26 19:56:54 -0500

    FIXED XML

--- watdo.js
+++ watdo.js
@@ -3,6 +3,17 @@ Events = new Mongo.Collection("events");
 if (Meteor.isClient) {
 	Template.body.helpers({
 		events: function() {
+            $.get("events.xml", function (data) {
+                $(data).find("item").each(function () { // or "item" or whatever suits your feed
+                    var el = $(this);
+                    Events.insert({
+                        title: el.find("title").text(),
+                        date: el.find("pubdate").text(),
+                        link: el.find("link").text(),
+                        description: el.find("description").text()
+                    });
+                });
+            });
     		return Events.find({});
     	}
   	});
@@ -10,19 +21,19 @@ if (Meteor.isClient) {
 
 if (Meteor.isServer) {
 	Meteor.startup(function () {
-    	var Twit = Meteor.npmRequire('twit');
-        var T = new Twit({
-            var items = {};
-            $.getJSON("secret.json", function(data) {
-                $.each(data, function(key, val) {
-                    items[key] = val;
-                });
-            });
-            consumer_key: items["consumer_key"],
-            consumer_secret: items["consumer_secret"],
-            access_token: items["access_token"],
-            access_token_secret: items["access_token_secret"]
-        });
+ //    	var Twit = Meteor.npmRequire('twit');
+ //        var items = {};
+ // //        $.getJSON("secret.json", function(data) {
+ // //            $.each(data, function(key, val) {
+ // //                items[key] = val;
+ // //            });
+ // //        });
+ // //        var T = new Twit({
+ // //            consumer_key: items["consumer_key"],
+ // //            consumer_secret: items["consumer_secret"],
+ // //            access_token: items["access_token"],
+ // //            access_token_secret: items["access_token_secret"]
+ // //        });
 	});
 }
 
